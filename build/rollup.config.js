be30172375a95546dfb6e48945db22c8bc850144
Jeff Anderson <janderson@nanthealth.com>
2020-03-06 16:13:00 -0500

    Fix Build

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,6 +1,6 @@
-import vue from 'rollup-plugin-vue2'; // Handle .vue SFC files
-import commonjs from '@rollup/plugin-commonjs'; // Convert CommonJS modules to ES6
-import buble from '@rollup/plugin-buble'; // Transpile/polyfill with reasonable browser support
+import commonjs from 'rollup-plugin-commonjs'; // Convert CommonJS modules to ES6
+import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
+import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
 export default {
     input: 'src/wrapper.js', // Path relative to package.json
     output: {
