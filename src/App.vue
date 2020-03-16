e24e1c0cc0ef8f586c313c76962988eccb8c8504
Jeff Anderson <janderson@nanthealth.com>
2020-03-16 16:10:05 -0400

    showSummary=true

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="app">
-		<provider-type-ahead name="servicing" label="Servicing Provider" :intervalInMS="700" :minLength="1" :showSummary="false"/>
-		<tx-provider-type-ahead name="tx-prov" label="Texas Provider" :intervalInMS="400" :minLength="1" :showSummary="false"/>
+		<provider-type-ahead name="servicing" label="Servicing Provider" :intervalInMS="700" :minLength="1" :showSummary="true"/>
+		<tx-provider-type-ahead name="tx-prov" label="Texas Provider" :intervalInMS="400" :minLength="1" :showSummary="true"/>
 	</div>
 </template>
 
